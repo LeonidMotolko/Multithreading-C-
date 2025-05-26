5a9680fb307f4cf9cccce9bb91f7a65245f1365d
LeonidMotolko <147403563+LeonidMotolko@users.noreply.github.com>
2025-05-26 13:09:51 +0300

    Update Tablet.cs

--- Lab1/task2/TablesLibrary/Tablet.cs
+++ Lab1/task2/TablesLibrary/Tablet.cs
@@ -1,1 +1,10 @@
-
+namespace TablesLibrary
+{
+    public class Tablet
+    {
+        private int ID { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+        public string OSType { get; set; }
+    }
+}
