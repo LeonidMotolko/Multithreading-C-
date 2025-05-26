8cffe2246ee9ec127800b742293389a47538df4d
LeonidMotolko <147403563+LeonidMotolko@users.noreply.github.com>
2025-05-26 13:11:44 +0300

    Update Manufacturer.cs

--- Lab1/task2/TablesLibrary/Manufacturer.cs
+++ Lab1/task2/TablesLibrary/Manufacturer.cs
@@ -1,1 +1,9 @@
-
+namespace TabletLibrary
+{
+  public class Manufacturer
+  {
+      public string Name { get; set; }
+      public string Address { get; set; }
+      private bool IsAChildCompany { get; set; }
+  }
+}
